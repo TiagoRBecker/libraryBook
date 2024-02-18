91b5cec7f82a49abfbdd667b285282524dbbed87
Tiago Becker <beckertiago09@gmail.com>
2024-02-18 18:44:31 -0300

    loading

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -3,7 +3,7 @@ import { ClipLoader } from "react-spinners";
 const Loading = () => {
     return (
     <section className="w-full h-screen flex items-center justify-center">
-     <img src="/loading.gif" alt="Loading" />
+     <img src="https://res.cloudinary.com/tiagobecker/image/upload/v1708292537/Loading_oe1xhc.gif" alt="Loading" />
     </section>  );
 }
  
