22b1007d3b5800c81cb6f8e1aa3a403f8d563447
Tiago Becker <beckertiago09@gmail.com>
2024-02-18 12:27:27 -0300

    grid

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -34,7 +34,8 @@ const Categories = async () => {
             </Link>
           </div>
 
-         
+           {
+            category.length >= 4 ?
             <Carrousel>
               {category.magazine.map((magazine, magazineIndex) => (
                 <Link href={`/magazine/${magazine.id}`} key={index} className="mb-5">
@@ -55,6 +56,30 @@ const Categories = async () => {
                 </Link>
               ))}
             </Carrousel>
+            :
+            <div className="w-full grid grid-cols-5">
+              {category.magazine.map((magazine, magazineIndex) => (
+                <Link href={`/magazine/${magazine.id}`} key={index} className="mb-5">
+                  <div
+                    key={magazineIndex}
+                    className=" w-[90%] h-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] transition ease-in-out delay-150 "
+                  >
+                    <img
+                      src={magazine.cover}
+                      alt={magazine.name}
+                      className="w-full h-[270px] object-fill"
+                    />
+                    <h1 className="text-black font-semibold pl-1">
+                      {magazine.name}
+                    </h1>
+                    <span className="pl-1">Edição {magazine.volume}</span>
+                  </div>
+                </Link>
+              ))}
+            </div>
+
+           }
+            
           
         </div>
       ))}
